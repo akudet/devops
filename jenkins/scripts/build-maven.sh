d07818d6b5ff06f7ff59a579a01e708adb65c9d7
Jintong Wu <akudet@qq.com>
2019-05-29 15:32:53 +0800

    jenkins : add k8s app name env

--- jenkins/scripts/build-maven.sh
+++ jenkins/scripts/build-maven.sh
@@ -4,11 +4,13 @@ export ROOT_PROJECT_NAME=$(gradle properties -q | grep "rootProject:" | awk '{pr
 export PROJECT_GROUP=$(gradle properties -q | grep "group:" | awk '{print $2}')
 export PROJECT_NAME=$(gradle properties -q | grep "name:" | awk '{print $2}')
 export PROJECT_VERSION=$(gradle properties -q | grep "version:" | awk '{print $2}')
+export PROJECT_MAJOR_VERSION=$(echo $PROJECT_VERSION | awk '{split($0,v,"."); print v[1]}')
 export PROJECT_JAR="$PROJECT_NAME-$PROJECT_VERSION.jar"
-export DOCKER_IMAGE=$DOCKER_SERVER/$ROOT_PROJECT_NAME/$PROJECT_NAME:$PROJECT_VERSION
+export DOCKER_IMAGE=$DOCKER_SERVER/$ROOT_PROJECT_NAME/$PROJECT_NAME:$PROJECT_VERSION-$BUILD_NUMBER
 export PROJECT_RELEASE='dev'
 export PROJECT_NAMESPACE='dev'
 export SERVER_PORT=8080
+export APP_NAME=$PROJECT_NAME-v$PROJECT_MAJOR_VERSION
 
 gradle build
 
