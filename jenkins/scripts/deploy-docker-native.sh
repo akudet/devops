a6a58e0052e68e52e455b4a5ba17c186497f72fe
Jintong Wu <akudet@qq.com>
2020-02-11 18:30:06 +0800

    jenkins : add ingress enable option

--- jenkins/scripts/deploy-docker-native.sh
+++ jenkins/scripts/deploy-docker-native.sh
@@ -13,17 +13,8 @@ if [ -f "jenkins/env.$DEPLOY_ENV" ]; then
   . jenkins/env.$DEPLOY_ENV
 fi
 
+## deploy docker
 export DEPLOY_NAME=${DEPLOY_NAME:-"$PROJECT_NAME"}
-
-INGRESS_PATH=${INGRESS_PATH:-"/$PROJECT_NAME"}
-
-printenv
-
-curl --insecure -X PUT "$GATEWAY_URL/build/updateZuulConfig" \
-  -d "project_name=$PROJECT_NAME" \
-  -d "project_path=$INGRESS_PATH/**" \
-  -d "project_url=http://$PROJECT_NAME/"
-
 DOCKER_RUN="docker run --name $DEPLOY_NAME -d --restart=always $DOCKER_OPTIONS -v run_env:/run/secrets/run_env:ro --network $DEPLOY_ENV -e SPRING_PROFILES_ACTIVE=$DEPLOY_ENV $DOCKER_IMAGE"
 
 echo $DOCKER_RUN
@@ -32,3 +23,15 @@ ssh -p $SSH_PORT $SSH_SERVER "docker login $DOCKER_SERVER"
 ssh -p $SSH_PORT $SSH_SERVER "docker rm -f $DEPLOY_NAME"
 ssh -p $SSH_PORT $SSH_SERVER "$DOCKER_RUN"
 ssh -p $SSH_PORT $SSH_SERVER "docker image prune -f -a --filter=label=app=$PROJECT_NAME"
+
+## add ingress mapping
+INGRESS_ENABLE=${INGRESS_ENABLE:-true}
+if $INGRESS_ENABLE; then
+  INGRESS_PATH=${INGRESS_PATH:-"/$PROJECT_NAME"}
+  curl --insecure -X PUT "$GATEWAY_URL/build/updateZuulConfig" \
+    -d "project_name=$PROJECT_NAME" \
+    -d "project_path=$INGRESS_PATH/**" \
+    -d "project_url=http://$PROJECT_NAME/"
+fi
+
+printenv
